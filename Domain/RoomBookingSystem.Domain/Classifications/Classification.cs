558099bc6d89013577e2d6c112c08905808cf26d
romesh-abeyawardena <romesh.abeyawardena@bellrockgroup.co.uk>
2024-04-26 14:28:14 +0100

    Added docker

--- Domain/RoomBookingSystem.Domain/Classifications/Classification.cs
+++ Domain/RoomBookingSystem.Domain/Classifications/Classification.cs
@@ -5,9 +5,14 @@ namespace RoomBookingSystem.Domain.Classifications
     public class Classification
     {
         public Guid ID { get; set; }
+        public Guid? CategoryId { get; set; }
+        public Guid? SubCategoryId {get;set;}
         public string? Name { get; set; }
         public string? DisplayName { get; set; }
         public Guid? ParentClassificationId { get; set; }
         public virtual Classification? ParentClassification { get; set; }
+
+        public virtual Categories.Category? Category { get; set; }
+        public virtual Categories.SubCategory? SubCategory { get; set; }
     }
 }
