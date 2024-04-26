e260bb4e1b9e37bbdc1c9f05db3c387c2f654cc8
Romesh Abeyawardena <romesh.a@outlook.com>
2024-04-26 20:53:55 +0100

    Added db context

---
+++ Infrastructure/RoomBookingSystem.Infrastructure/Areas/RoomBookingSystemDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace RoomBookingSystem.Infrastructure
+{
+    public partial class RoomBookingSystemDbContext
+    {
+        public DbSet<Domain.Areas.Area> Areas { get; set; }
+        public DbSet<Domain.Areas.AreaBooking> AreaBookings { get; set; }
+        public DbSet<Domain.Areas.AreaRestriction> AreaRestrictions { get; set; }
+        public DbSet<Domain.Areas.AreaSubUse> AreaSubUse { get; set; }
+        public DbSet<Domain.Areas.AreaType> AreaTypes { get; set; }
+        public DbSet<Domain.Areas.AreaUse> AreaUses { get; set; }
+        public DbSet<Domain.Areas.AreaWorkNotice> AreaWorkNotices { get; set; }
+    }
+}
---
+++ Infrastructure/RoomBookingSystem.Infrastructure/Assets/RoomBookingSystemDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RoomBookingSystem.Domain.Assets;
+
+namespace RoomBookingSystem.Infrastructure
+{
+    public partial class RoomBookingSystemDbContext
+    {
+        public DbSet<Equipment> Equipment { get; set; }
+        public DbSet<EquipmentLocation> EquipmentLocations { get; set; }
+    }
+}
